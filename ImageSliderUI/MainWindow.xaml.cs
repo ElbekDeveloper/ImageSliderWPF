c44066219890dedb8fc19e69d874e78106117cc6
ElbekDeveloper <e.normurodov@mail.ru>
2020-06-09 19:52:16 +0500

    complete the slider

--- ImageSliderUI/MainWindow.xaml.cs
+++ ImageSliderUI/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace ImageSliderUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int indexOfImage { get; set; } = 1;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            indexOfImage--;
+            if (indexOfImage < 1)
+            {
+                indexOfImage = 4;
+            }
+
+            picHolder.Source = new BitmapImage(new Uri(@"pics/" + indexOfImage + ".jpg", UriKind.Relative));
+        }
+
+        private void Next(object sender, RoutedEventArgs e)
+        {
+            indexOfImage++;
+            if (indexOfImage > 4)
+            {
+                indexOfImage = 1;
+            }
+
+            picHolder.Source = new BitmapImage(new Uri(@"pics/" + indexOfImage + ".jpg", UriKind.Relative));
         }
     }
 }
